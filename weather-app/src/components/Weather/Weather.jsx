ac3c8f03ab9fa2bf71edc85ced978b27b58e26c1
Karen Celis <k-celis@javeriana.edu.co>
2021-05-25 12:02:01 -0500

    add material icons , assess the state  , and create a list

--- weather-app/src/components/Weather/Weather.jsx
+++ weather-app/src/components/Weather/Weather.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
-import { WiCloud } from 'react-icons/wi';
+import { WiCloud, WiDayCloudy, WiDayFog, WiDaySunny, WiRain } from 'react-icons/wi';
 import { IconContext } from 'react-icons';
 
-const Weather = ({ temperature }) => {
+//objeto
+const stateByName = {
+  cloud: WiCloud,
+  cloudy: WiDayCloudy,
+  fog: WiDayFog,
+  sunny: WiDaySunny,
+  rain: WiRain,
+};
+
+const renderState = (state) => {
+  //primera forma de hacerlo
+  // let Icon = stateByName[state];
+  // if (Icon === undefined) {
+  //   Icon = stateByName['sunny'];
+  // }
+//segunda forma
+  const Icon = stateByName[state] === undefined ? stateByName['sunny'] : stateByName[state];
+  //const Icon = stateByName[state] && stateByName[state];
+  return <Icon />;
+};
+const Weather = ({ temperature, state }) => {
   return (
     <div>
+
       <IconContext.Provider value={{ size: '5em' }}>
-        <WiCloud />
+        {renderState(state)}
       </IconContext.Provider>
 
       <Typography display='inline' variant='h3'>{temperature}</Typography>
@@ -18,6 +39,7 @@ const Weather = ({ temperature }) => {
 
 Weather.propTypes = {
   temperature: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired,
 };
 
 export default Weather;
