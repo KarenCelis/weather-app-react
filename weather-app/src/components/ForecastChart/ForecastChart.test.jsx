1e991a013b8700e9c3a100693cc664933cef4127
Karen Celis <k-celis@javeriana.edu.co>
2021-05-26 09:45:07 -0500

    ForecastChart Component

---
+++ weather-app/src/components/ForecastChart/ForecastChart.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ForecastChart from './ForecastChart';
+import '@testing-library/jest-dom/extend-expect';
+
+test('ForecastChart render', async () => {
+
+    // const { findAllByTestId } = render(<Forecast forecastItemList={forecastItemList} />);
+    // const forecastItems = await findAllByTestId('forecast-item-container');
+    // expect(forecastItems).toHaveLength(7);
+
+  });
